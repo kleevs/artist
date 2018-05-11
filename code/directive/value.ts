9ea0cad72b489e8defac0fd47b49358b116aad61
Kevin Landa <kev.sam.landa@gmail.com>
2018-05-11 12:08:39 +0200

    directive value

--- code/directive/value.ts
+++ code/directive/value.ts
@@ -1,7 +1,9 @@
 import { on } from 'on';
 import { Binder } from '../core/view';
 
-export function value(options: { get: () => string, set: (value: string) => void, on?: string}) : Binder[] { 
+export function value(options: { get: () => boolean, set: (value: boolean) => void, on?: string}) : Binder[];
+export function value(options: { get: () => string, set: (value: string) => void, on?: string}) : Binder[];
+export function value(options: { get: () => string|boolean, set: (value: string|boolean) => void, on?: string}) : Binder[] { 
 	return [
 		on(options.on || 'input', () => (e) => { 
 			var target: any = <any>e.currentTarget;
--- src/directive/value.d.ts
+++ src/directive/value.d.ts
@@ -1,5 +1,10 @@
 import { Binder } from '../core/view';
 export declare function value(options: {
+    get: () => boolean;
+    set: (value: boolean) => void;
+    on?: string;
+}): Binder[];
+export declare function value(options: {
     get: () => string;
     set: (value: string) => void;
     on?: string;
